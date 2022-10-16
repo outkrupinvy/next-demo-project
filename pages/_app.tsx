0677354f365072e19281f543f130fcd74750b4d4
@Poel_pospal_happy <krupin.vitaliy@mail.ru>
2022-10-16 20:52:31 +0300

    save project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { AppProps } from 'next/app'
 
+import '@/assets/styles/globals.scss'
+
+import AuthProvider from '../app/providers/auth-provider/AuthProvider'
+import { ThemeProvider } from '../app/providers/theme-provider/ThemeProvider'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false
+		}
+	}
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<QueryClientProvider client={queryClient}>
+			<AuthProvider>
+				<ThemeProvider>
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</AuthProvider>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	)
 }
 
 export default MyApp
